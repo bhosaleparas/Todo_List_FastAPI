caadd72d9eb41038cc621d0bbe0134db65ed1dd5
bhosaleparas <parasbhosale375@gmail.com>
2025-12-17 00:25:10 +0530

    managed code

--- crud.py
+++ crud.py
@@ -4,15 +4,24 @@ import schemas
 from auth import get_password_hash
 from typing import List, Optional
 
+
+
 # User CRUD operations
 def get_user_by_id(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+
+
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+
+
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = get_password_hash(user.password)
@@ -25,6 +34,8 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
 
 # Todo CRUD operations
 def get_todo(db: Session, todo_id: int, user_id: int):
@@ -47,12 +58,16 @@ def get_todos(
     
     return query.offset(skip).limit(limit).all()
 
+
+
 def create_todo(db: Session, todo: schemas.TodoCreate, user_id: int):
     db_todo = models.Todo(**todo.dict(), user_id=user_id)
     db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
     return db_todo
+
+
 
 def update_todo(db: Session, todo_id: int, todo_update: schemas.TodoUpdate, user_id: int):
     db_todo = get_todo(db, todo_id, user_id)
@@ -67,6 +82,8 @@ def update_todo(db: Session, todo_id: int, todo_update: schemas.TodoUpdate, user
     db.refresh(db_todo)
     return db_todo
 
+
+
 def delete_todo(db: Session, todo_id: int, user_id: int):
     db_todo = get_todo(db, todo_id, user_id)
     if not db_todo:
@@ -75,3 +92,4 @@ def delete_todo(db: Session, todo_id: int, user_id: int):
     db.delete(db_todo)
     db.commit()
     return True
+
